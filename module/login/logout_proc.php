92b24c829bf594c2e88e7ab05b9a5e6ec315022d
바가나 <86461360+whynot1217@users.noreply.github.com>
2023-06-27 12:29:40 +0900

    Delete module directory

--- module/login/logout_proc.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-unset($_SESSION["ses_member_userid"]);
-unset($_SESSION["ses_member_name"]);
-unset($_SESSION["ses_member_type"]);
-unset($_SESSION["ses_member_site"]);
-
-header("Location:/");
